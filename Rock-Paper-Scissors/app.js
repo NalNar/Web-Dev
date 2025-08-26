eae8d8b9d04c1763e5e6d49d9161b50b4d0737c5
Nalin Narayan <160528770+NalNar@users.noreply.github.com>
2025-08-26 23:55:57 +0000

    update

--- Rock-Paper-Scissors/app.js
+++ Rock-Paper-Scissors/app.js
@@ -1,15 +1,17 @@
-const computerChoice = document.getElementById('computerChoice');
-const yourChoice = document.getElementById('yourChoice');
-const resultDisplay = document.getElementById('results');
-const possibleChoices = document.querySelectorAll('button');
-let userChoice;
+document.addEventListener('DOMContentLoaded', () => {
+    const computerChoice = document.getElementById('computerChoice');
+    const yourChoice = document.getElementById('yourChoice');
+    const resultDisplay = document.getElementById('results');
+    const possibleChoices = document.querySelectorAll('button');
+    let userChoice;
 
-possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-    userChoice = e.target.id;
-    yourChoice.innerHTML = userChoice;
-}));
+    possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+        userChoice = e.target.id;
+        yourChoice.innerHTML = userChoice;
+    }));
 
-function generateComputerChoice() {
-    const randomNumber = Math.floor(Math.random() * possibleChoices.length); // using * 3 
-    console.log(randomNumber);
-}
+    function generateComputerChoice() {
+        const randomNumber = Math.floor(Math.random() * possibleChoices.length);
+        console.log(randomNumber);
+    }
+});
