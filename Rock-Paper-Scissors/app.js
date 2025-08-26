c60e9891931ed96e6dc064ce535d605d165b9a43
Nalin Narayan <160528770+NalNar@users.noreply.github.com>
2025-08-26 23:46:18 +0000

    Update

---
+++ Rock-Paper-Scissors/app.js
@@ -0,0 +1,17 @@
+const computerChoice = document.getElementById('computer-choice');
+const yourChoice = document.getElementById('your-choice');
+const resultDisplay = document.getElementById('results');
+const possibleChoices = document.querySelectorAll('button');
+let userChoice;
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id;
+    yourChoice.innerHTML = userChoice;
+}));
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length); // using * 3 
+    console.log(randomNumber);
+
+
+}
