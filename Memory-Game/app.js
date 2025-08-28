ae2421c88a36de9b78b998b479998c302379502b
Nalin Narayan <160528770+NalNar@users.noreply.github.com>
2025-08-28 04:19:59 +0000

    add

---
+++ Memory-Game/app.js
@@ -0,0 +1,52 @@
+const cardArray =[
+    {
+        name: 'fries',
+        img:'images/fries.png'
+    },
+    {
+        name:'cheeseburger',
+        img:'images/cheeseburger.png'
+    },
+    {
+        name:'hotdog',
+        img:'images/hotdog.png'
+    },
+    {
+        name:'ice-cream',
+        img: 'images/ice-cream'
+    },
+    {
+        name:'milkshake',
+        img: 'images/milkshake'
+    },
+    {
+        name:'pizza',
+        img: 'images/pizza'
+    }
+{
+        name: 'fries',
+        img:'images/fries.png'
+    },
+    {
+        name:'cheeseburger',
+        img:'images/cheeseburger.png'
+    },
+    {
+        name:'hotdog',
+        img:'images/hotdog.png'
+    },
+    {
+        name:'ice-cream',
+        img: 'images/ice-cream'
+    },
+    {
+        name:'milkshake',
+        img: 'images/milkshake'
+    },
+    {
+        name:'pizza',
+        img: 'images/pizza'
+    }
+];
+
+console.log(cardArray);
